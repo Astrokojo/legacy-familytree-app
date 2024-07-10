a4c533115a5b31539d89b2fadbad6a1a68e02baa
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-07-10 20:30:09 +0000

    feat: add Footer component and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function App() {
     return (
         <React.Fragment>
             <Navbar />
-            <Contact/>
+            <Contact />
+            <Footer/>
         </React.Fragment>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <div id='footer'>
+            <div className='primary-heading'>
+                <p>
+                Legacy Family Tree Portfolio project. <br/>
+                    &copy; 2024 Kojo Antwi Osei-Sarfo 
+                </p>
+            </div>
+        </div>
+
+    )
+}
+export default Footer;
