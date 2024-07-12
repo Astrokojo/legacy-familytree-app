69786cad089a842de75338030839224b7cfa1739
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-07-12 01:25:01 +0000

    feat: add GOJS diagram(TreeDiagram)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
+import TreeDiagram from './components/TreeDiagram'
 export default function App() {
     return (
         <React.Fragment>
             <Navbar />
             <Contact />
-            <Footer/>
+            <Footer />
+            <TreeDiagram/>
         </React.Fragment>
     );
 }
