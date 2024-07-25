934387976b49c369676bdbd117b1d1bc75c086cd
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-07-25 20:21:36 +0000

    refactor: Add lazy loading for images in FamilyTree and Hero components

--- familytree/src/components/Hero.js
+++ familytree/src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
             variants={slideIn("right", 0.1)}
             initial="hidden"
             whileInView={"show"}
-            viewport={{ once: true, amount: 0.7 }}
+            viewport={{ once: false, amount: 1 }}
             className="hero">
             <div className="hero-mask">
                 <motion.div
