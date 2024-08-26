2f0e2aa345ceacc47051bca4f2c05310d078e5cd
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-08-26 04:52:47 +0000

    refactor: Update FamilyDetails component to use useState hook for modelData and setModelData
    The FamilyDetails component has been updated to use the useState hook for managing the modelData state and the setModelData function. This change improves code readability and simplifies the management of the component's data.

--- familytree/src/components/FamilyDetails.js
+++ familytree/src/components/FamilyDetails.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { LOCAL_API } from "../config/apiEndpoints";
 import useFetch from "../hooks/useFetch";
@@ -5,27 +6,39 @@ import ClearDataButton from "../utils/clearDataButton";
 import { fadeIn, slideIn } from "../utils/variants";
 import FamilyForm from "./FamilyForm";
 import FamilyList from "./FamilyList";
+import { v4 as uuidv4 } from 'uuid';
+
+
 function FamilyDetails() {
-  const { error, isPending, data: modelData } = useFetch(LOCAL_API);
+  const { error, isPending, data: modelDataFromAPI } = useFetch(LOCAL_API);
+
+  const [modelData, setModelData] = useState([
+    { key: uuidv4(), name: '', gender: '', birthYear: '', deathYear: '', parent: '' }
+  ]);
+
+
 
   return (
     <div className="family-details">
-
       <motion.div
         variants={slideIn("left", 0.1)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{ once: false, amount: 0.9 }} className="family-details-header">
+        viewport={{ once: false, amount: 0.9 }}
+        className="family-details-header">
         <h2>Help Us Build Your Family Tree</h2>
       </motion.div>
 
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
+
+
       <motion.div
         variants={fadeIn("left", 0.2)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{ once: false, amount: 0.9 }}><FamilyForm />
+        viewport={{ once: false, amount: 0.9 }}>
+        <FamilyForm modelData={modelData} setModelData={setModelData} />
       </motion.div>
 
       {modelData && <FamilyList modelData={modelData} />}
